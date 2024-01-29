6b5f3d5a767b829462be7fd64e623afbd14c8afd
Mohammed-Junaid01 <156078869+Mohammed-Junaid01@users.noreply.github.com>
2024-01-29 19:43:53 +0530

    index.html added

---
+++ index.js
@@ -0,0 +1,61 @@
+// ------------javaScript for Toggle Menu-------
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0"
+}
+function hideMenu(){
+    navLinks.style.right = "-200px"
+}
+
+
+// ---------------tab-links--------------
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+
+
+
+
+
+
+
+
+// ------------------FAQ,s-----------
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+
